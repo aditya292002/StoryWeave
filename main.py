9921f45ce1d881af72c942ae3337ca028986b0a8
Aditya Keshari <keshariaditya90@gmail.com>
2024-01-08 19:48:50 +0530

    nf

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def home():
         
         room = code
         if create != False:
-            room = generate_unique_code(4)
+            room = generate_unique_code(10)
             rooms[room] = {"members": 0, "messages": []}
         elif code not in rooms:
             return render_template("home.html", error="Room does not exist.", code=code, name=name)
